6e998a0ee6d09f653f8357bc25d704017f01d7d8
Gabriela Papp <gpgabika@gmail.com>
2022-01-21 16:39:26 +0000

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../index.css'
+
+const Footer = () => {
+  return (
+    <div>
+      <h1 className="text-6xl font-bold underline">Hello world!</h1>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../index.css'
+
+const Routes = () => {
+  return (
+    <div>
+      <h1 className="text-6xl font-bold underline">Hello world!</h1>
+    </div>
+  )
+}
+
+export default Routes
